55041564627a87253e4842a01f1d5ae089f11d46
XhantheProg <146364321+XhantheProg@users.noreply.github.com>
2023-11-21 00:27:19 -0400

    proyecto actualizacion
    añadi los datos de algunos libros en libros

--- Proyecto.sql
+++ Proyecto.sql
@@ -15,85 +15,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for empresa
-CREATE DATABASE IF NOT EXISTS `empresa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `empresa`;
+-- Dumping database structure for venta
+CREATE DATABASE IF NOT EXISTS `venta` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `venta`;
 
--- Dumping structure for table empresa.asignacion_tarea
-CREATE TABLE IF NOT EXISTS `asignacion_tarea` (
-  `id_empleado` int unsigned NOT NULL,
-  `id_tarea` int unsigned NOT NULL,
-  PRIMARY KEY (`id_empleado`,`id_tarea`),
-  KEY `id_tarea` (`id_tarea`),
-  CONSTRAINT `asignacion_tarea_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id`),
-  CONSTRAINT `asignacion_tarea_ibfk_2` FOREIGN KEY (`id_tarea`) REFERENCES `tarea` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table empresa.cliente
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `id_departamento` int unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_departamento` (`id_departamento`),
-  CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table empresa.departamento
-CREATE TABLE IF NOT EXISTS `departamento` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `presupuesto` double unsigned NOT NULL,
-  `gastos` double unsigned NOT NULL,
+-- Dumping structure for table venta.libreria
+CREATE TABLE IF NOT EXISTS `libreria` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table empresa.empleado
-CREATE TABLE IF NOT EXISTS `empleado` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `nif` varchar(9) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `apellido1` varchar(100) NOT NULL,
-  `apellido2` varchar(100) DEFAULT NULL,
-  `id_departamento` int unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `nif` (`nif`),
-  KEY `id_departamento` (`id_departamento`),
-  CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table empresa.proyecto
-CREATE TABLE IF NOT EXISTS `proyecto` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `descripcion` text,
-  `id_departamento` int unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_departamento` (`id_departamento`),
-  CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table empresa.tarea
-CREATE TABLE IF NOT EXISTS `tarea` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `descripcion` text,
-  `id_proyecto` int unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_proyecto` (`id_proyecto`),
-  CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Dumping structure for table venta.libro
+CREATE TABLE IF NOT EXISTS `libro` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `NombreDelLibro` varchar(50) DEFAULT NULL,
+  `Categoria` varchar(50) DEFAULT NULL,
+  `Autor` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
